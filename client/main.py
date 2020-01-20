b3fe82fb1557af606d81e1cdb9b1bd6db3d8ad9a
Slonov <aleksey.slonov@mail.ru>
2020-01-20 09:50:55 +0600

    bug fix

--- client/main.py
+++ client/main.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     factory.protocol = MyClientProtocol
 
     loop = asyncio.get_event_loop()
-    coro = loop.create_connection(factory, '127.0.0.1:9090', 9090)
+    coro = loop.create_connection(factory, '127.0.0.1', 9090)
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
